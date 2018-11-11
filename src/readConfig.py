608300a98b73862e94b33503dccac701dc46cb79
ahmetesatgenc <a.esatgenc@gmail.com>
2018-11-11 16:37:44 +0300

    Changed due to the optional features
    Transformation side is now optinol. Hence, readConfig file is changed accordingly.

--- src/readConfig.py
+++ src/readConfig.py
@@ -1,9 +1,11 @@
 features = ['Feature Detector','Descriptor Extractor','Transformation','Image Matching']
-keywords_featureDetector = ['FAST','STAR','SIFT','SURF','ORB','BRISK','MSER','SimpleBlob']
+keywords_featureDetector = ['FAST','STAR','SIFT','SURF','ORB','BRISK','SimpleBlob']
 keywords_descriptorExtractor = ['SIFT','SURF','BRIEF','BRISK','ORB','FREAK']
 keywords_transformation = ['Affine Transform','Perspective Transform']
 keywords_IM = ['Histogram Matching','Template Matching','Pixel Matching']
 image_matching = []*3
+
+
 
 def get_Feature():
 
@@ -19,18 +21,24 @@ def get_Feature():
         if all_features[index_of_FD+index_of_featureDetector+2] == '1':
             feature_detector = keywords_featureDetector[k]
             break
+        else:
+            feature_detector = None
 
     for k in range (0,len(keywords_descriptorExtractor)):
         index_of_descriptorExtractor = all_features[index_of_DE:index_of_T].index(keywords_descriptorExtractor[k])
         if all_features[index_of_DE+index_of_descriptorExtractor+2] == '1':
             descriptor_extractor = keywords_descriptorExtractor[k]
             break
+        else:
+            descriptor_extractor = None
 
     for k in range (0,len(keywords_transformation)):
         index_of_transformation = all_features[index_of_T:index_of_IM].index(keywords_transformation[k])
         if all_features[index_of_T+index_of_transformation+2] == '1':
             transformation = keywords_transformation[k]
             break
+        else:
+            transformation = None
 
     for k in range (0,len(keywords_IM)):
         index_of_imageMatching = all_features[index_of_IM:].index(keywords_IM[k])
